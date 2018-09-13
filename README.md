1cb3697234bb8830498bf12664b0bb3317566812
Martin Melo <alejandromelo2005@gmail.com>
2018-09-12 22:24:08 -0300

    Not Completed. Need to fix the filter menu because does not looks well on cellphones.

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# Hotels
-Hotels Fullstack test
+# WEB Test
+
+## Dependencies
+
+Git
+NodeJs 8 or Greater
+NPM
+
+## Clone, Build & Run
+
+```
+git clone https://github.com/MartinMelo/Hotels
+cd Hotels
+npm install
+cd public
+npm install
+ng build
+cd ..
+npm start
+```
+
+Runs at port 80
+
+## Requirements
+
+The requirements of this project are the following [REQUIREMENTS.pdf](requirements.pdf) file for details
+
+## License
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
+
+## Author
+
+* **Martin Melo** - *Repositories:* - [Martin Melo](https://github.com/MartinMelo)
+
--- routes/hotels.js
+++ routes/hotels.js
@@ -7,5 +7,32 @@ router.get('/all', function(req, res) {
     var json = JSON.parse(fs.readFileSync('hotels.json', 'utf8'));
     res.send(json);
 });
+router.get('/byStars', function(req, res) {
+    var fs = require('fs');
+    var json = JSON.parse(fs.readFileSync('hotels.json', 'utf8'));
+    var starsToFilter = parseInt(req.query.stars);
+    console.log(starsToFilter);
+    var filteredHotels = getFilterHotelByStars(json, starsToFilter);
+    res.send(filteredHotels);
+});
+router.get('/byName', function(req, res) {
+    var fs = require('fs');
+    var json = JSON.parse(fs.readFileSync('hotels.json', 'utf8'));
+    var nameToFilter = req.query.name.toLowerCase();
+    var filteredHotels = getFilterHotelsByName(json, nameToFilter);
+    res.send(filteredHotels);
+});
+
+function getFilterHotelByStars(hotels,stars) {
+    return hotels.filter(function(e) {
+        return e.stars === stars;
+    });
+}
+function getFilterHotelsByName(hotels, nameToSearch) {
+    return hotels.filter(function(e) {
+        return e.name.toLowerCase().indexOf(nameToSearch) > -1;
+    });
+}
+
 
 module.exports = router;
